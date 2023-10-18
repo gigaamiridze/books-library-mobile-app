bffd75ed70ee47e9e17305edf2def429a0ee2a93
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-19 00:27:13 +0400

    Added heading title for book selection screen

--- src/screens/Selection.tsx
+++ src/screens/Selection.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { FlexBox, WelcomeSection } from '../components';
+import { FlexBox, WelcomeSection, Heading } from '../components';
+import { Fonts, Routes } from '../constants';
 import { globalStyles } from '../styles';
-import { Routes } from '../constants';
 
 function Selection() {
   const navigation = useNavigation();
@@ -12,7 +12,7 @@ function Selection() {
     <SafeAreaView style={[globalStyles.flex, globalStyles.backgroundWhite]}>
       <FlexBox 
         flexDirection='column' 
-        rowGap={40}
+        rowGap={60}
       >
         <WelcomeSection 
           smallTitle='Explore your favorite books'
@@ -20,6 +20,16 @@ function Selection() {
           backTitle='Back To Category Page'
           onBack={() => navigation.navigate(Routes.CATEGORIES)}
         />
+        <View style={globalStyles.paddingHorizontal}>
+          <FlexBox flexDirection='column' alignItems='center' rowGap={40}>
+            <Heading
+              title='Select a book'
+              type={2}
+              fontFamily={Fonts.SECONDARY}
+              textAlign='center'
+            />
+          </FlexBox>
+        </View>
       </FlexBox>
     </SafeAreaView>
   )
