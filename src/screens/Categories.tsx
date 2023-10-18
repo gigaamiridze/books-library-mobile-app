95ef888fe0f0adf6db5e07ee4556a6da3483a1e8
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-18 20:27:04 +0400

    Generated book categories

--- src/screens/Categories.tsx
+++ src/screens/Categories.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import { FlexBox, WelcomeSection, Heading } from '../components';
-import { Fonts, Routes } from '../constants';
+import { SafeAreaView, ScrollView, View, ActivityIndicator, FlatList } from 'react-native';
+import { FlexBox, WelcomeSection, Heading, Button } from '../components';
+import { Colors, Fonts, LibraryActions, Routes } from '../constants';
+import { useLibraryContext } from '../contexts';
+import { getBookCategories } from '../api';
 import { globalStyles } from '../styles';
 
 function Categories() {
+  const [page, setPage] = useState<number>(0);
   const navigation = useNavigation();
+  const { libraryState: { selectedCategory }, dispatchLibrary } = useLibraryContext();
+
+  const {
+    data: categories,
+    isLoading,
+    isError,
+    isFetching,
+    isPreviousData,
+  } = useQuery(['categories', page], () => getBookCategories(page, 10), {
+    keepPreviousData: true,
+    staleTime: 300000, // 5 minutes in milliseconds
+    cacheTime: 3600000, // 1 hour in milliseconds
+  });
+
+  const handleCategorySelection = (newCategory: string) => {
+    dispatchLibrary({ type: LibraryActions.SELECT_CATEGORY, payload: newCategory });
+  }
 
   return (
     <SafeAreaView style={[globalStyles.flex, globalStyles.backgroundWhite]}>
-      <ScrollView 
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 90 }}
-      >
-        <FlexBox 
-          flexDirection='column' 
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <FlexBox
+          flexDirection='column'
           rowGap={60}
         >
-          <WelcomeSection 
+          <WelcomeSection
             smallTitle='Explore your favorite book category'
             bigTitle='Categories'
             backTitle='Back To Home'
@@ -32,6 +50,31 @@ function Categories() {
                 fontFamily={Fonts.SECONDARY}
                 textAlign='center'
               />
+              {isLoading || isFetching ? (
+                <ActivityIndicator size='large' color={Colors.PURPLE} />
+              ) : isError ? (
+                <Heading
+                  title='Something went wrong. Please try again.'
+                  type={5}
+                  fontWeight='500'
+                />
+              ) : (
+                <FlatList 
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerStyle={{ columnGap: 10 }}
+                  data={categories}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({ item }) => (
+                    <Button
+                      key={item.id}
+                      title={item.title}
+                      isSelected={selectedCategory === item.title}
+                      onPress={() => handleCategorySelection(item.title)}
+                    />
+                  )}
+                />
+              )}
             </FlexBox>
           </View>
         </FlexBox>
