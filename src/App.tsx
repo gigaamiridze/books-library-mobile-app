d2da1e1ce1001420ee31cedaf80c8fb88b900dc7
Giga Amiridze <gigaamiridze@gmail.com>
2023-10-18 19:51:34 +0400

    Implemented query client provider using react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { AlertNotificationRoot } from 'react-native-alert-notification';
 import { UserContextProvider, LibraryContextProvider } from './contexts';
 import { StackNavigation } from './navigation';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
     <UserContextProvider>
       <LibraryContextProvider>
-        <AlertNotificationRoot>
-          <NavigationContainer>
-            <StackNavigation />
-          </NavigationContainer>
-        </AlertNotificationRoot>
+        <QueryClientProvider client={queryClient}>
+          <AlertNotificationRoot>
+            <NavigationContainer>
+              <StackNavigation />
+            </NavigationContainer>
+          </AlertNotificationRoot>
+        </QueryClientProvider>
       </LibraryContextProvider>    
     </UserContextProvider>
   );
